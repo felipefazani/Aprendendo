ee354c4218403c126a0cf36cfba5e2d6d05533d4
Felipe Fazani <lipefazani@gmail.com>
2021-08-24 12:37:46 -0300

    Criando 01estrutura
    Criando a primeira estrutura em C.
    Adicionando alguns arquivos em Funcoes\Fatorial

---
+++ Estruturas/1estrutura/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct Aluno{
+    int RA;
+    int idade;
+};
+
+struct Aluno leitura();
+
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+struct Aluno leitura(){
+    
+}
