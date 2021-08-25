5439ce05aa1861f8988939c653d40ce2cde68386
Felipe Fazani <lipefazani@gmail.com>
2021-08-25 10:13:40 -0300

    Terminando o exercício

--- PHP/Introducao/02ifelse.php
+++ PHP/Introducao/02ifelse.php
@@ -1,3 +1,13 @@
 <?php 
-   // Crie um algoritmo que leia dois valores (x e y) representando um intervalo. Em seguida, leia um novo valor (z) e verifique se z pertence ao intervalo [x,y].
+   // Crie um algoritmo que leia dois valores (x e y) representando um intervalo. 
+   // Em seguida, leia um novo valor (z) e verifique se z pertence ao intervalo [x,y].
+
+   $x = readline("Digite o valor de X: ");
+   $y = readline("Digite o valor de Y: ");
+   $z = readline("Digite o valor de Z: ");
+
+   if(($z >= $x) && ($z <= $y))
+      echo "Z está dentro do intervalo [X,Y]";
+   else
+      echo "Z não pertence ao intervalo [X, Y]";
 ?>
