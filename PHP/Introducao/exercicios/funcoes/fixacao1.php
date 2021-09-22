4ae197a29a9fb5b23ab04a99812e1e922d9c768b
Felipe Fazani <lipefazani@gmail.com>
2021-09-22 10:55:29 -0300

    Create fixacao1.php

---
+++ PHP/Introducao/exercicios/funcoes/fixacao1.php
@@ -0,0 +1,6 @@
+<?php 
+   // preencher um vetor com numeros inteiros (8unidades); solicitar um numero
+   // do teclado. Pesquisar se esse numero existe no vetor. Se existir, imprimir
+   // em qual posicao do vetor. Se nao existir, imprimir mensagem que nao existe.
+   
+?>
