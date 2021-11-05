0802f5bc94dde359b79762acee7eb80f4c7088ab
Felipe Fazani <lipefazani@gmail.com>
2021-11-05 19:57:37 -0300

    markparametrize

--- PYTHON/expressao/test.py
+++ PYTHON/expressao/test.py
@@ -2,16 +2,12 @@ import main
 import pytest
 
 class Test:
+   @pytest.mark.parametrize('entrada, valor_esperado', [
+      ('4 + 4 - 2 * 3', '2.0'),
+      ('3 + 2 - 7', '-2.0'),
+      ('0 * 2 - 0', '0.0'),
+      ('7 * 9 * 0 + 3', '3.0')
+   ])
 
-   @pytest.fixture
-   def obj(self):
-      return main.Calcula_expressao()
-   
-   def exp1(self, obj):
-      assert obj.expressao('4 + 4 - 2 * 3') == 2
-      assert obj.expressao('2 * 2 * 2 0') == 80
-      assert obj.expressao('2 / 2 * 2 0') == 20
-      assert obj.expressao('3 + 2 - 7') == -2
-      assert obj.expressao('0 * 2 - 0') == 0
-      assert obj.expressao('7 * 9 * 0 + 3') == 3
-
+   def test_expressao(self, entrada, valor_esperado):
+      assert main.Calcula_expressao().expressao(entrada) == valor_esperado
