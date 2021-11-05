2ff6a3d85d866687cf930f196fb19375d54b656b
Felipe Fazani <lipefazani@gmail.com>
2021-11-05 19:19:42 -0300

    transformando main em classe

--- PYTHON/expressao/main.py
+++ PYTHON/expressao/main.py
@@ -1,48 +1,49 @@
-def expressao(ex):
-   '''recebe uma expressao matematica em forma de lista e retorna seu resultado,
-      nao funciona com parenteses, potenciacao e radiciacao'''
+class Calcula_expressao:
+   def expressao(self, ex):
+      '''recebe uma expressao matematica em forma de lista e retorna seu resultado,
+         nao funciona com parenteses, potenciacao e radiciacao'''
 
-   ex = [x for x in ex] # transformar a string em lista
-   while(ex.count(' ') > 0): # retirar todos espacos em branco
-      ex.remove(' ')
-   calcula_exp('*', '/', ex)
-   calcula_exp('+', '-', ex)
-   return ex[0]
-   
+      ex = [x for x in ex] # transformar a string em lista
+      while(ex.count(' ') > 0): # retirar todos espacos em branco
+         ex.remove(' ')
+      self.calcula_exp('*', '/', ex)
+      self.calcula_exp('+', '-', ex)
+      return ex[0]
+      
 
-def existe(op, list):
-   try:
-      list.index(op)
-      return True
-   except ValueError:
-      return False
+   def existe(self, op, list):
+      try:
+         list.index(op)
+         return True
+      except ValueError:
+         return False
 
-def calcula_op(op, list):
-   x = list.index(op)
-   if op == '+':
-      resul = float(list[x-1]) + float(list[x+1])
-   elif op == '-':
-      resul = float(list[x-1]) - float(list[x+1])
-   elif op == '*':
-      resul = float(list[x-1]) * float(list[x+1])
-   elif op == '/':
-      resul = float(list[x-1]) / float(list[x+1])
-   list[x-1] = str(resul)
-   list[x:x+2] = []
+   def calcula_op(self, op, list):
+      x = list.index(op)
+      if op == '+':
+         resul = float(list[x-1]) + float(list[x+1])
+      elif op == '-':
+         resul = float(list[x-1]) - float(list[x+1])
+      elif op == '*':
+         resul = float(list[x-1]) * float(list[x+1])
+      elif op == '/':
+         resul = float(list[x-1]) / float(list[x+1])
+      list[x-1] = str(resul)
+      list[x:x+2] = []
 
-def calcula_exp(op1, op2, list):
-   while(op1 in list or op2 in list):
-      if existe(op1, list) and existe(op2, list):
-         # faz o que vier primeiro
-         if list.index(op1) < list.index(op2):
+   def calcula_exp(self, op1, op2, list):
+      while(op1 in list or op2 in list):
+         if self.existe(op1, list) and self.existe(op2, list):
+            # faz o que vier primeiro
+            if list.index(op1) < list.index(op2):
+               # multiplica
+               self.calcula_op(op1, list)
+            else:
+               # divide
+               self.calcula_op(op2, list)
+         elif self.existe(op1, list):
             # multiplica
-            calcula_op(op1, list)
+            self.calcula_op(op1, list)
          else:
             # divide
-            calcula_op(op2, list)
-      elif existe(op1, list):
-         # multiplica
-         calcula_op(op1, list)
-      else:
-         # divide
-         calcula_op(op2, list)
+            self.calcula_op(op2, list)
