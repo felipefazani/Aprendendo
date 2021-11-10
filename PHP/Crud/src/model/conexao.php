e5386d938ef37c48b5d391a907c47e9f5676cc2e
Felipe Fazani <lipefazani@gmail.com>
2021-11-10 08:26:13 -0300

    Create conexao.php

---
+++ PHP/Crud/src/model/conexao.php
@@ -0,0 +1,16 @@
+<?php 
+
+$host = 'localhost';
+$usuario = 'root';
+$senha = '';
+$db = 'lanchonete1';
+
+$conexao = new mysqli($host, $usuario, $senha, $db);
+
+if($conexao->connect_errno){
+   die ("Erro: $conexao->connect_errno");
+}else{
+   echo "conexao ok";
+}
+
+?>
