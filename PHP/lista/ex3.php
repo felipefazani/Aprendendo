d72234965626ce9755d62178266e0ca021d965be
Felipe Fazani <lipefazani@gmail.com>
2021-11-23 10:40:18 -0300

    Create ex3.php

---
+++ PHP/lista/ex3.php
@@ -0,0 +1,8 @@
+<?php 
+   //  Crie uma função em linguagem PHP chamado Dado() que retorna, através de 
+   //  sorteio, um número de 1 até 6.
+   function Dado(){
+      return rand(1, 6);
+   }
+   print(Dado());
+?>
