a6560cb905ca25dda8a60e2a6e86433705747565
Felipe Fazani <lipefazani@gmail.com>
2021-11-24 10:46:31 -0300

    Update ex5.php
    colocando o exercicio

--- PHP/lista/ex5.php
+++ PHP/lista/ex5.php
@@ -1,4 +1,12 @@
 <?php 
+//  Crie um aplicativo de conversão entre as temperaturas Celsius e Farenheit.
+//  Primeiro o usuário deve escolher se vai entrar com a temperatura em Célsius ou 
+//  Farenheit, depois a conversão escolhida é realizada através de um comando 
+//  SWITCH.
+//  Se C é a temperatura em Célsius e F em farenheit, as fórmulas de conversão são:
+//  C= 5.(F-32)/9
+//  F= (9.C/5) + 32
+ 
    $op = readline("Para transformar Fahrenheit em Celsius digite 1 para o contrário digite 2:");
    switch ($op) {
       case 1:
